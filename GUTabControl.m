4e9443c78849ea5e47cb9e4334a93c58f07680a4
sidetlw <tlw.isit@gmail.com>
2018-02-02 14:38:55 +0800

    Update GUTabControl.m

--- GUTabControl.m
+++ GUTabControl.m
@@ -3,7 +3,7 @@
 //  GearUI
 //
 //  Created by sidetang on 2017/10/1.
-//
+//  Copyright © 2017年 Tencent home voice assitance. All rights reserved.
 
 #import "GUTabControl.h"
 
@@ -28,10 +28,11 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
-        _unselectedColor = [UIColor whiteColor];
+        _unSelectedColor = [UIColor whiteColor];
         _selectedColor = [UIColor whiteColor];
         _indicatorColor = [UIColor whiteColor];
         _isFirst = false;
+        
     }
     return self;
 }
@@ -40,37 +41,38 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
     [self updateSubViews];
 }
 
-- (void)setCurrentPage:(NSInteger)currentPage
+- (void)setSelectedIndex:(NSInteger)selectedIndex
 {
-    _currentPage = currentPage;
+    _selectedIndex = selectedIndex;
     [self setNeedsLayout];
     for (UIView *view in self.subviews) {
         if ([view isKindOfClass:[UIButton class]]) {
             UIButton *button = (UIButton *)view;
-            if ([button.titleLabel.text isEqualToString:_titles[self.currentPage]]) {
+            if ([button.titleLabel.text isEqualToString:_items[self.selectedIndex]]) {
                 [button setTitleColor:self.selectedColor forState:UIControlStateNormal];
             }
             else {
-                [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                [button setTitleColor:self.unSelectedColor forState:UIControlStateNormal];
             }
         }
     }
-    
 }
 
 - (void)setSelectedColor:(UIColor *)color{
     _selectedColor = color;
     [self setNeedsLayout];
+//    [self setSelectedIndex:_selectedIndex];
 }
 
 - (void)setUnSelectedColor:(UIColor *)color {
-    _unselectedColor = color;
+    _unSelectedColor = color;
     [self setNeedsLayout];
+//    [self setSelectedIndex:_selectedIndex];
 }
 
-- (void)setTitles:(NSArray<NSString *> *)titles {
+- (void)setItems:(NSArray<NSString *> *)titles {
     NSArray<NSString *> *array = [titles copy];
-    _titles = array;
+    _items = array;
     
     for (UIView *view in self.subviews) {
         if (view != _indicatorView) {
@@ -78,24 +80,28 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
         }
     }
     
-    for (int i = 0; i < _titles.count; i++) {
-        CGSize size = self.bounds.size;
-        CGFloat buttonWidth = size.width / (CGFloat)_titles.count;
+    CGSize size = self.bounds.size;
+    CGFloat buttonWidth = size.width / (CGFloat)_items.count;
+    for (int i = 0; i < _items.count; i++) {
+       
+        
         UIButton* button = [[UIButton alloc] initWithFrame:CGRectMake(buttonWidth * (CGFloat)i, 0, buttonWidth, size.height)];
         button.autoresizingMask = UIViewAutoresizingFlexibleHeight;
-        [button setTitle:[_titles objectAtIndex:i] forState:UIControlStateNormal];
+        [button setTitle:[_items objectAtIndex:i] forState:UIControlStateNormal];
         [button.titleLabel setFont:[UIFont boldSystemFontOfSize:17]];
         button.tag = 100 + i;
         [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:button];
     } //for
+    
+    _originCenter = CGPointMake(buttonWidth / 2.0, self.bounds.size.height - 1);
 }
 
 - (void)buttonTapped:(id)sender {
     if ([sender isKindOfClass:[UIButton class]]) {
         UIButton *button = (UIButton *)sender;
-        NSInteger index = [_titles indexOfObject:button.titleLabel.text];
-        self.currentPage = index;
+        NSInteger index = [_items indexOfObject:button.titleLabel.text];
+        self.selectedIndex = index;
         [self sendActionsForControlEvents:UIControlEventValueChanged];
     }
 }
@@ -110,13 +116,8 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
 }
 
 - (void)updateSubViews {
-    CGFloat buttonWidth = self.bounds.size.width / (CGFloat)_titles.count;
-    self.indicatorView.center = CGPointMake(buttonWidth * (_currentPage + 1) - (buttonWidth / 2.0), self.bounds.size.height - 1);
- 
-    if (self.isFirst == false && _indicatorView.center.x < buttonWidth) {
-        self.isFirst = true;
-        _originCenter = _indicatorView.center;
-    }
+    CGFloat buttonWidth = self.bounds.size.width / (CGFloat)_items.count;
+    self.indicatorView.center = CGPointMake(buttonWidth * (_selectedIndex + 1) - (buttonWidth / 2.0), self.bounds.size.height - 1);
 }
 
 -(void)setIncatorViewCenterWithFactor:(CGFloat)factor direction:(NSInteger)direct {
@@ -130,9 +131,13 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
     
     _direction = direct;
     
-    CGFloat buttonWidth = self.bounds.size.width / (CGFloat)_titles.count;
-    CGFloat delta = (self.bounds.size.width - (0.5 * buttonWidth) - _originCenter.x) * factor;
-    CGFloat finalX = _originCenter.x + delta;
+    CGFloat buttonWidth = self.bounds.size.width / (CGFloat)_items.count;
+    CGFloat delta = buttonWidth * factor;
+    NSInteger index = self.selectedIndex;
+    if (self.selectedIndex >= 1 && direct == 1) {
+        index = self.selectedIndex - 1;
+    }
+    CGFloat finalX = _originCenter.x + delta + (buttonWidth * index);
     _indicatorView.center = CGPointMake(finalX, _originCenter.y);
     
     [self setTitleColorGradual:factor];
@@ -140,38 +145,38 @@ typedef NS_ENUM(NSInteger, GNScrollDirection) {
 
 -(void)setTitleColorGradual:(CGFloat)factor {
     if (self.direction == GNScrollDirectionLeft) {
-        if (self.currentPage == self.titles.count - 1) {
+        if (self.selectedIndex == self.items.count - 1) {
             return;
         }
         
         for (UIView *view in self.subviews) {
             if ([view isKindOfClass:[UIButton class]]) {
                 UIButton *button = (UIButton *)view;
-                if ([button.titleLabel.text isEqualToString:_titles[self.currentPage]]) {
-                    UIColor *gradiulColor = [self colorBetween:self.selectedColor and:[UIColor whiteColor] distance:factor];
+                if ([button.titleLabel.text isEqualToString:_items[self.selectedIndex]]) {
+                    UIColor *gradiulColor = [self colorBetween:self.selectedColor and:self.unSelectedColor distance:factor];
                     [button setTitleColor:gradiulColor forState:UIControlStateNormal];
                 }
-                else if ([button.titleLabel.text isEqualToString:_titles[self.currentPage + 1]]) {
-                    UIColor *gradiulColor = [self colorBetween:[UIColor whiteColor]  and:self.selectedColor  distance:factor];
+                else if ([button.titleLabel.text isEqualToString:_items[self.selectedIndex + 1]]) {
+                    UIColor *gradiulColor = [self colorBetween:self.unSelectedColor  and:self.selectedColor distance:factor];
                     [button setTitleColor:gradiulColor forState:UIControlStateNormal];
                 }
             }
         }
     }
     else if (self.direction == GNScrollDirectionRight) {
-        if (self.currentPage == 0) {
+        if (self.selectedIndex == 0) {
             return;
         }
         
         for (UIView *view in self.subviews) {
             if ([view isKindOfClass:[UIButton class]]) {
                 UIButton *button = (UIButton *)view;
-                if ([button.titleLabel.text isEqualToString:_titles[self.currentPage]]) {
-                    UIColor *gradiulColor = [self colorBetween:self.selectedColor and:[UIColor whiteColor] distance:1.0 - factor];
+                if ([button.titleLabel.text isEqualToString:_items[self.selectedIndex]]) {
+                    UIColor *gradiulColor = [self colorBetween:self.selectedColor and:self.unSelectedColor distance:1.0 - factor];
                     [button setTitleColor:gradiulColor forState:UIControlStateNormal];
                 }
-                else if ([button.titleLabel.text isEqualToString:_titles[self.currentPage - 1]]) {
-                    UIColor *gradiulColor = [self colorBetween:[UIColor whiteColor]  and:self.selectedColor  distance:1.0 - factor];
+                else if ([button.titleLabel.text isEqualToString:_items[self.selectedIndex - 1]]) {
+                    UIColor *gradiulColor = [self colorBetween:self.unSelectedColor  and:self.selectedColor distance:1.0 - factor];
                     [button setTitleColor:gradiulColor forState:UIControlStateNormal];
                 }
             }
