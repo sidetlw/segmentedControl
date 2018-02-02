f7245eb0eaffdd26abeb3fb30d3e334a426b5b34
sidetlw <tlw.isit@gmail.com>
2018-02-02 14:37:38 +0800

    Update GUTabControl.h

--- GUTabControl.h
+++ GUTabControl.h
@@ -3,19 +3,19 @@
 //  GearUI
 //
 //  Created by sidetang on 2017/10/1.
-//
+//  Copyright © 2017年 Tencent home voice assitance. All rights reserved.
 
 #import <UIKit/UIKit.h>
 
 @interface GUTabControl : UIControl
 
-@property (nonatomic, copy) NSArray<NSString *> *titles;
+@property (nonatomic, copy) NSArray<NSString *> *items;
 
-@property (nonatomic) NSInteger currentPage;
+@property (nonatomic) NSInteger selectedIndex;
 
 @property (nonatomic, strong) UIColor *selectedColor;
 
-@property (nonatomic, strong) UIColor *unselectedColor;
+@property (nonatomic, strong) UIColor *unSelectedColor;
 
 @property (nonatomic, strong) UIColor *indicatorColor;
 
